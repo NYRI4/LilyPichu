2f64d1eddde3e94598ff400602a86b0fc0c74a10
NYRI4 <kilianmalherbe.pro@gmail.com>
2023-03-08 06:10:52 +0100

    Changing classes

---
+++ stuff/overlay.css
@@ -0,0 +1,78 @@
+:root .app-3xd6d0::before, :root .app-3xd6d0::after, :root .app-2CXKsg::after, :root .app-2CXKsg::before, :root .layers-OrUESM::before, :root .layers-OrUESM::after, :root .app-3xd6d0::before, :root .bg-1QIAus::after, :root .bg-1QIAus::before {
+  content: "";
+  position: fixed;
+  z-index: 1;
+  pointer-events: none;
+}
+:root .app-3xd6d0::before {
+  left: 0;
+  top: 0;
+  width: 250px;
+  height: 250px;
+  background: url("https://i.imgur.com/jTpFJuI.png") no-repeat;
+  background-size: 100%;
+}
+:root .app-3xd6d0::after {
+  z-index: 0;
+  left: 50px;
+  bottom: 0;
+  width: 490px;
+  height: 40px;
+  background: url("https://i.imgur.com/KQQWcTY.png") no-repeat;
+  background-size: 100%;
+}
+:root .app-2CXKsg::before {
+  top: 80px;
+  right: 116px;
+  width: 200px;
+  height: 120px;
+  background: url("https://i.imgur.com/0nsnP69.png") no-repeat;
+  background-size: 100%;
+}
+:root .app-2CXKsg::after {
+  bottom: 20px;
+  right: 45%;
+  width: 40px;
+  height: 40px;
+  background: url("https://i.imgur.com/OBSWYht.png");
+  background-size: 100%;
+}
+:root .layers-OrUESM::before {
+  z-index: 0;
+  bottom: 75px;
+  right: 0;
+  width: 20px;
+  height: 50px;
+  background: url("https://i.imgur.com/WveiFXb.png") no-repeat;
+  background-size: 100%;
+}
+:root .layers-OrUESM::after {
+  bottom: 15px;
+  right: 100px;
+  width: 25px;
+  height: 25px;
+  background: url("https://i.imgur.com/WBUag7e.png") no-repeat;
+  background-size: 100%;
+}
+:root .bg-1QIAus::after {
+  top: 45px;
+  right: 385px;
+  width: 30px;
+  height: 30px;
+  background: url("https://i.imgur.com/aOYvahh.png") no-repeat;
+  background-size: 100%;
+}
+:root .bg-1QIAus::before {
+  z-index: 2;
+  bottom: 30%;
+  right: 430px;
+  width: 30px;
+  height: 40px;
+  background: url("https://i.imgur.com/JMoXBIs.png") no-repeat;
+  background-size: 100%;
+}
+@media (max-width: 1350px) {
+  :root .bg-1QIAus::before {
+    display: none;
+  }
+}
